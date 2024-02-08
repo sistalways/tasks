93c741d7d0698af79ed0b854360c3a83ecc52887
Girish Sista <sistalways@wifi-roaming-128-4-92-150.wifi.udel.edu>
2024-02-08 16:08:00 -0500

    task 1 pt.2

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="APP">
-            <header className="App-Header">
-                UD CISC275 with React Hooks and TypeScript
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript Girish Sista
             </header>
             <p>
-                Hello World Girish Sista<code>src/App.tsx</code> This page will
-                automatically reload.
+                Edit <code>src/App.tsx</code> and save to reload. Edit{" "}
+                <code>src/App.tsx</code> and save. This page will automatically
+                reload.
             </p>
         </div>
     );
