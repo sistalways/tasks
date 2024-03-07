093667729aa0a65ec7c88d98091bf2430a8d45fd
Girish Sista <vjgcs@udel.edu>
2024-03-07 13:13:39 -0500

    task 10 pt2

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,46 +1,6 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-interface editProp {
-    isStudent: boolean;
-    setIsStudent: (b: boolean) => void;
-    name: string;
-    setName: (b: string) => void;
-}
-
-export function EMode({
-    name,
-    isStudent,
-    setName,
-    setIsStudent
-}: editProp): JSX.Element {
-    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
-        setIsStudent(event.target.checked);
-    }
-    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
-        setName(event.target.value);
-    }
-    return (
-        <div>
-            <Form.Check
-                type="checkbox"
-                id="Student-check"
-                label="Is Student?"
-                checked={isStudent}
-                onChange={updateStudent}
-            />
-            <Form.Group controlId="">
-                <Form.Label>Name:</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows={3}
-                    value={name}
-                    onChange={updateName}
-                />
-            </Form.Group>
-        </div>
-    );
-}
 export function EditMode(): JSX.Element {
     const [editMode, setEditMode] = useState<boolean>(false);
     const [name, setName] = useState<string>("your name");
@@ -48,6 +8,12 @@ export function EditMode(): JSX.Element {
 
     function updateEMode(event: React.ChangeEvent<HTMLInputElement>) {
         setEditMode(event.target.checked);
+    }
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
     }
     return (
         <>
@@ -69,14 +35,26 @@ export function EditMode(): JSX.Element {
                 </div>
                 <div>
                     {editMode ? (
-                        <EMode
-                            name={name}
-                            isStudent={isStudent}
-                            setName={setName}
-                            setIsStudent={setIsStudent}
-                        ></EMode>
+                        <>
+                            <Form.Check
+                                type="checkbox"
+                                id="Student-check"
+                                label="Is Student?"
+                                checked={isStudent}
+                                onChange={updateStudent}
+                            />
+                            <Form.Group controlId="">
+                                <Form.Label>Name:</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={3}
+                                    value={name}
+                                    onChange={updateName}
+                                />
+                            </Form.Group>
+                        </>
                     ) : (
-                        " "
+                        ""
                     )}
                 </div>
                 <div>
